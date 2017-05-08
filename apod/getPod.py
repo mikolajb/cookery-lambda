26ab3a980976866d6b5ae4cd73504b742dd7e678
tdobber <timo.dobber@student.uva.nl>
2017-05-08 14:45:27 +0200

    Did some reworking, function can be invoked

--- apod/getPod.py
+++ apod/getPod.py
@@ -1,4 +1,3 @@
-#getPod.py on Lambda server
 import urllib
 import boto3
 import datetime
@@ -6,28 +5,31 @@ import datetime
 s3 = boto3.client('s3')
 
 #Gets the picture of the day from the nasa site and stores it in Amazon s3 bucket
-def getPod(event, context):
+def handler(event, context):
 
-    page = urllib.urlopen(event['url'])
+	url = "https://apod.nasa.gov/apod/"
+	page = urllib.urlopen(url)
 
-    image_line = ""
+	image_line = ""
 
-    for line in page:
-        if ".jpg" in line:
-            image_line = line
-            break
+	for line in page:
+		if ".jpg" in line:
+			image_line = line
+			break
 
-    ref = image_line.split('"', 2)
+	ref = image_line.split('"', 2)
 
-    url_pic = "https://apod.nasa.gov/apod/" + ref[1]
-    # date = datetime.datetime.now()
-    # filename = "nasa%s%s%s.jpg" % (date.day, date.month, date.year)
-    filename = event["filename"]
-    file_location = "/tmp/" + filename
-    
-    file = open(file_location, 'w')
-    urllib.urlretrieve(url_pic, file_location)
-    bucket_name = event["bucketname"]
+	url_pic = url + "" + ref[1]
+	date = datetime.datetime.now()
+	file_name = "nasa%s%s%s.jpg" % (date.day, date.month, date.year)
+	# file_name = event["filename"]
+	file_location = "/tmp/" + file_name
+	
+	f = open(file_location, 'w')
+	urllib.urlretrieve(url_pic, file_location)
+	f.close()
+	# bucket_name = event["bucketname"]
+	bucket_name = "apodresult"
 
-    s3.upload_file(file_location, bucket_name, filename)
-    return 
+	s3.upload_file(file_location, bucket_name, file_name)
+	return ("Done! Image can be found in the s3 bucket: " + bucket_name + " as: " + file_name)
--- deploy/lambda_deploy.py
+++ deploy/lambda_deploy.py
@@ -1,44 +1,40 @@
-# todo: fix dependencies and libraries etc, check handler (name, form etc..) and get names of handler and file
+# todo: fix dependencies and libraries etc, set schedule(, with apod--> return picture??)
 
 import sys
 import os
 import boto3
 import zipfile
 
-ACCESS_KEY = "***REMOVED***"
-SECRET_KEY = "***REMOVED***"
+ACCESS_KEY = ""
+SECRET_KEY = ""
 REGION = "eu-central-1"
-BUCKET_NAME = "***REMOVED***"
+BUCKET_NAME = ""
 
+# Get the paramaters needed for deploying a function.
+# Works only if handler file is in top of directory...(check if handler function is correct?)
 # todo: make it work from everywhere, instead of cwd
-def getDirToDeploy():
-	if (len(sys.argv) < 2 or not os.path.isdir("./" + sys.argv[1])):
+def getParams():
+	if len(sys.argv) < 5:
+		sys.exit("Please run as follows: python3 --directory_to_deploy --function_name --handler_file_name --handler_function_name")
+	elif not os.path.isdir("./" + sys.argv[1]):
 		sys.exit("Please specify an existing directory.")
-	else:
-		return sys.argv[1]
-
-def getFunctionName():
-	if (len(sys.argv) < 3 or len(sys.argv[2]) > 64):
+	elif len(sys.argv[2]) > 64:
 		sys.exit("Please specify a proper function name with max 64 characters.")
-	else:
-		return sys.argv[2]
+	elif not os.path.isfile("./" + sys.argv[1] + "/" + sys.argv[3]):
+		sys.exit("Please specify an existing handler file name.")
 
-def setupLambdaClient():
-	client = boto3.client("lambda",
+	return (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+# Setup an AWS service.
+def setupClient(service):
+	client = boto3.client(service,
 		aws_access_key_id = ACCESS_KEY,
 		aws_secret_access_key = SECRET_KEY,
 		region_name = REGION)
 
 	return client
 
-def setupS3Client():
-	client = boto3.client("s3",
-		aws_access_key_id = ACCESS_KEY,
-		aws_secret_access_key = SECRET_KEY,
-		region_name = REGION)
-
-	return client
-
+# Check if there already exists a function with the same name.
 def checkExistingFunctions(client, function_name):
 	function_list = client.list_functions()
 	for i in range(0, len(function_list["Functions"])):
@@ -62,18 +58,29 @@ def uploadToS3(client, zip_name, bucket_name):
 	client.upload_file(zip_name, bucket_name, zip_name)
 	return
 
-def deployLambda(client, dir_to_deploy, function_name, zip_name):
+def invokeFunction(client, function_name):
+	response = client.invoke(
+		FunctionName = function_name,
+		InvocationType = "RequestResponse",
+		LogType = "None"
+	)
+	result = response["Payload"].read()
+	print(result)
+	return
+
+# Deploy the function to AWS Lambda.
+def deployLambda(client, dir_to_deploy, function_name, zip_name, file_name, handler_name):
 	response = client.create_function(
 		FunctionName = function_name,
-		Runtime = "python3.6",
+		Runtime = "python2.7",
 		Role = "arn:aws:iam::151587718953:role/service-role/testRole",
-		Handler = zip_name[:4] + "/test.testHandler",
+		Handler = dir_to_deploy + "/" + file_name[:-3] + "." + handler_name, # handler = file_name.handler_function_name
 		Code = {
 			"S3Bucket": BUCKET_NAME,
 			"S3Key": zip_name
 		},
 		Description = "",
-		Timeout = 3,
+		Timeout = 10,
 		MemorySize = 128,
 		Publish = False,
 		VpcConfig = {
@@ -93,17 +100,17 @@ def deployLambda(client, dir_to_deploy, function_name, zip_name):
 		Tags = {
 		}
 	)
-	print(response)
+	# print(response)
 	print("Done!")
 
 if __name__ == "__main__":
-	dir_to_deploy = getDirToDeploy()
-	function_name = getFunctionName()
+	(dir_to_deploy, function_name, file_name, handler_name) = getParams()
 
-	lambda_client = setupLambdaClient()
+	lambda_client = setupClient("lambda")
 	checkExistingFunctions(lambda_client, function_name)
 
 	zip_name = zipDirectory(dir_to_deploy)
-	s3_client = setupS3Client()
+	s3_client = setupClient("s3")
 	uploadToS3(s3_client, zip_name, BUCKET_NAME)
-	deployLambda(lambda_client, dir_to_deploy, function_name, zip_name)
+	deployLambda(lambda_client, dir_to_deploy, function_name, zip_name, file_name, handler_name)
+	invokeFunction(lambda_client, function_name)
